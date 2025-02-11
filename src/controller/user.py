7c9d00286b46aa11f530e23390d579186226e932
David <david.lopez.rivera.2003@gmail.com>
2025-02-10 21:59:17 -0500

    user.py correction

--- src/controller/user.py
+++ src/controller/user.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import psycopg2
 from psycopg2.extras import execute_values
 import sys
@@ -5,6 +6,7 @@ import os
 import pandas as pd
 sys.path.append("DepoHunter_paradigmas/src")
 import controller.SecretConfig as secretconfig
+import model.model_tables as models 
 
 
 class BaseController:
@@ -48,7 +50,8 @@ class BaseController:
         finally:
             cursor_lodging.close()
 
-    def PostTable(self, query: str, data: str) -> None:
+    
+    def PostTable(self, query, data):
         try:
             data_frame = pd.read_csv(data)
             cursor_lodging = self.connection.cursor()
@@ -66,8 +69,24 @@ class BaseController:
         finally:
             if 'cursor_lodging' in locals():
                 cursor_lodging.close()
+    
+    def PostTableOneElement(self,query):
+        try: 
+            cursor_lodging = self.connection.cursor()
+            cursor_lodging.execute(query)
+            self.connection.commit()
+            print("Elemento insertado exitosamente.")
+        except psycopg2.Error as e:
+            print("Error al insertar elemento en la tabla:", e)
+        finally:
+            cursor_lodging.close()
+    
+    
+    def UpdateTable():
+        pass     
+    def Gettable():
+        pass
                 
-
 class ControllerLodging:
     def __init__(self):
         self.base_controller = BaseController()
@@ -98,25 +117,40 @@ class ControllerLodging:
                     VALUES %s"""
         self.base_controller.PostTable(query, data)
 
+    def PostOneElement(self,element):
+        query=f"""
+        Insert into lodging(name,city,latitude,longitude,
+                            price,type,capacity,rooms_number,
+                            bathrooms_number ,bedrooms_number,
+                            id_host)
+        values
+        ('{element.name}', '{element.city}' ,'{element.latitude}','{element.longitude}',
+        {element.price},'{element.type}',{element.capacity},{element.rooms_number},
+        {element.bathrooms_number},{element.bedrooms_number},{element.id_host}
+        );"""
+        self.base_controller.PostTableOneElement(query=query)
 
 class ControllerUser:
     def __init__(self):
         self.base_controller=BaseController()
     
     def CreateTableUser(self):
-        query=""" CREATE TABLE IF NOT EXISTS user(
-                name varchart(50) not null,
+        query="""CREATE TABLE IF NOT EXISTS users(
+                name varchar(50) not null,
                 password varchar(50) not null
                 );"""
         self.base_controller.CreateTable(query=query)
     
     def DeleteTableUser(self):
-        query="""DROP TABLE IF EXISTS user"""
+        query="""DROP TABLE IF EXISTS users"""
         self.base_controller.DeleteTable(query=query)
     
-    def PostTableUser():
-        pass
-
+    def PostTableUser(self,element):
+        query=f""" insert into users(name,password)
+                    values
+                    ('{element.name}','{element.password}');
+        """
+        self.base_controller.PostTableOneElement(query=query)
 
 class ControllerReservation:
     def __init__(self):
@@ -127,17 +161,20 @@ class ControllerReservation:
             id_reservation SERIAL PRIMARY KEY,
             id_lodging INT NOT NULL,
             initial_date TIMESTAMP NOT NULL,
-            end_date TIMESTAMP NOT NULL,
-        );     
-        """
+            end_date TIMESTAMP NOT NULL
+        );"""
         self.base_controller.CreateTable(query=query)
     
     def DeleteTableReservation(self): 
         query="""DROP TABLE IF EXISTS reservation"""
         self .base_controller.DeleteTable(query=query)
     
-    def PostTableReservation(self):
-        pass
+    def PostTableReservation(self,element):
+        query=f""" insert into reservation(id_lodging,initial_date, end_date)
+                    values({element.id_lodging},'{element.initial_date}','{(element.end_date)}'
+                    );
+        """
+        self.base_controller.PostTableOneElement(query=query)
 
 class ControllerImage:
     def __init__(self):
@@ -145,7 +182,7 @@ class ControllerImage:
     
     def CreateTableImage(self):
         query=""" CREATE TABLE IF NOT EXISTS Image(
-        id_imagen SERIAL PRIMARY KEY,
+        id_image SERIAL PRIMARY KEY,
         lodging varchar(50) not null,
         address varchar(50) not null
         );
@@ -154,8 +191,12 @@ class ControllerImage:
 
     def DeleteTableImage(self):
         query="""DROP TABLE IF EXISTS Image"""
-        self.base_controller.DeleteTable(query=query)    
+        self.base_controller.DeleteTable(query=query)
 
+    def postImage(self,element):
+        query=f""" insert into Image(lodging,address)
+        values('{element.lodging}','{element.lodging}');"""
+        self.base_controller.PostTableOneElement(query=query)    
 
 class Controllerhost:
     def __init__(self):
@@ -171,9 +212,11 @@ class Controllerhost:
     def DeleteTableHost(self):
         query="""DROP TABLE IF EXISTS host"""
         self.base_controller.DeleteTable(query=query)
-
     
-
+    def posthost(self,element):
+        query=f"""insert into host(host)
+        values({element.host});"""
+        self.base_controller.PostTableOneElement(query=query)
 
 
 
