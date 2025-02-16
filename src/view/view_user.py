8c95226b646644571462f0f39f762f19495a0971
Mmolina07 <mateoma1234@gmail.com>
2025-02-16 18:45:02 -0500

    view_user Done

--- src/view/view_user.py
+++ src/view/view_user.py
@@ -1,15 +1,49 @@
 from flask import Blueprint, render_template
+import sys
+import os
+sys.path.append("src")
+import controller.Controller_host as Controller_host
+import controller.Controller_Image as Controller_Image
+import controller.Controller_Lodging as  Controller_Lodging
+import controller.Controller_Reservation as Controller_Reservation
+import controller.Controller_Result as Controller_Result
+import controller.Controller_user as Controller_user
+import controller.Controller_Review as  Controller_Review
+
+instance_controller_host=Controller_host.Controllerhost()
+instance_controller_Image=Controller_Image.ControllerImage()
+instance_controller_Lodging=Controller_Lodging.ControllerLodging()
+instance_controller_Reservation=Controller_Reservation.ControllerReservation()
+instance_controller_Result=Controller_Result.ControllerResult()
+instance_controller_user=Controller_user.ControllerUser()
+instance_controller_Review=Controller_Review.ControllerReview()
+
+instance_controller_host.CreateTableHost()
+instance_controller_Image.CreateTableImage()
+instance_controller_Lodging.CreateTableLodging()
+instance_controller_Reservation.CreateTableReservation()
+instance_controller_user.CreateTableUser()
+
+instance_controller_host.PostDataHost(data="src/utils/host_data.csv")
+instance_controller_Image.PostDataImage(data="src/utils/urls.csv")
+instance_controller_Lodging.PostDataLodging(data="src/utils/df_lodging.csv")
+instance_controller_Review.PostDataReview(data="src/utils/sample_reviews.csv")
+
 
 blueprint = Blueprint( "view_user", __name__, template_folder="templates" )
 
-alojamientos = [
-   {"id": 100, "nombre": "Alojamiento1", "precio": 123000, "imagen1": "https://www.revistaaxxis.com.co/wp-content/uploads/2022/08/casa_cinco_9.jpg", "imagen2":"https://arquitectopablorestrepo.com/wp-content/uploads/2024/01/Casa-Moderna-Costa-Azul-6.jpg", "imagen3" : "https://revistaaxxis.com.co/wp-content/uploads/2019/09/llano-axxis1.jpg"},
-   {"id": 101, "nombre": "Alojamiento2", "precio": 150000, "imagen1": "https://www.revistaaxxis.com.co/wp-content/uploads/2022/08/casa_cinco_9.jpg", "imagen2":"https://arquitectopablorestrepo.com/wp-content/uploads/2024/01/Casa-Moderna-Costa-Azul-6.jpg", "imagen3" : "https://revistaaxxis.com.co/wp-content/uploads/2019/09/llano-axxis1.jpg"},
-   {"id": 102, "nombre": "Alojamiento3", "precio": 95000, "imagen1": "https://www.revistaaxxis.com.co/wp-content/uploads/2022/08/casa_cinco_9.jpg", "imagen2":"https://arquitectopablorestrepo.com/wp-content/uploads/2024/01/Casa-Moderna-Costa-Azul-6.jpg", "imagen3" : "https://revistaaxxis.com.co/wp-content/uploads/2019/09/llano-axxis1.jpg"}
-]
+alojamientos = instance_controller_Result.filterdefault()
+# [
+#   {"id": 100, "nombre": "Alojamiento1", "precio": 123000, "imagen1": "https://www.revistaaxxis.com.co/wp-content/uploads/2022/08/casa_cinco_9.jpg", "imagen2":"https://arquitectopablorestrepo.com/wp-content/uploads/2024/01/Casa-Moderna-Costa-Azul-6.jpg", "imagen3" : "https://revistaaxxis.com.co/wp-content/uploads/2019/09/llano-axxis1.jpg"},
+#   {"id": 101, "nombre": "Alojamiento2", "precio": 150000, "imagen1": "https://www.revistaaxxis.com.co/wp-content/uploads/2022/08/casa_cinco_9.jpg", "imagen2":"https://arquitectopablorestrepo.com/wp-content/uploads/2024/01/Casa-Moderna-Costa-Azul-6.jpg", "imagen3" : "https://revistaaxxis.com.co/wp-content/uploads/2019/09/llano-axxis1.jpg"},
+#   {"id": 102, "nombre": "Alojamiento3", "precio": 95000, "imagen1": "https://www.revistaaxxis.com.co/wp-content/uploads/2022/08/casa_cinco_9.jpg", "imagen2":"https://arquitectopablorestrepo.com/wp-content/uploads/2024/01/Casa-Moderna-Costa-Azul-6.jpg", "imagen3" : "https://revistaaxxis.com.co/wp-content/uploads/2019/09/llano-axxis1.jpg"}
+#]
+
+
 
 @blueprint.route("/")
 def home():
+   #alojamientos_=instance_controller_Result.filterdefault()
    return render_template("home.html", alojamientos=alojamientos)
 
 @blueprint.route("/alojamiento/<int:id>")
@@ -21,12 +55,16 @@ def alojamiento_detalle(id):
 
 @blueprint.route('/pago')
 def pago():
-    return render_template('pago.html')
+   return render_template('pago.html')
 
-@blueprint.route("/Login")
-def Login():
+@blueprint.route("/login")
+def login():
    return render_template("login.html")
 
-@blueprint.route("/Register")
-def Register():
+@blueprint.route("/register")
+def register():
    return render_template("register.html")
+
+@blueprint.route("/reservations")
+def reservations():
+   return render_template("reservations.html")
